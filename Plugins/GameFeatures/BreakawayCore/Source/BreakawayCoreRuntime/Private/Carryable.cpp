ea060ba7931818f0ee3f12607611d871de315e9f
Steve <steveulrich@gmail.com>
2025-02-25 11:57:34 -0800

    Added lots of assets and logic around the relic/relic pickup/CTF plugin and spawners. Removed lots of early attempts at code

---
+++ Plugins/GameFeatures/BreakawayCore/Source/BreakawayCoreRuntime/Private/Carryable.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Carryable.h"
+
+
+// Add default functionality here for any ICarryable functions that are not pure virtual.
